ef0321061782fd192aef6d379454458f7f46c5d1
SyalsabillaValentisyesa <syalsabillavalentisyesa_2327250054@mhs.mdp.ac.id>
2025-03-05 09:23:02 +0700

    lanjutt pilem

--- pilem/lib/main.dart
+++ pilem/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:pilem/screens/home_screen.dart';
+import 'package:pilem/screens/favorite_screen.dart';
+import 'package:pilem/screens/search_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/detail_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -7,13 +10,58 @@ void main() {
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: HomeScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+  final List<Widget> _screen = [
+    HomeScreen(),
+    SearchScreen(),
+    FavoriteScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screen[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favorite',
+            ),
+          ]),
     );
   }
 }
--- pilem/lib/sevies/api_services.dart
+++ pilem/lib/sevies/api_services.dart
@@ -37,4 +37,12 @@ class ApiServices {
     final data = json.decode(response.body);
     return List<Map<String, dynamic>>.from(data['result']);
   }
+
+  Future<List<Map<String, dynamic>>> searchMovies(String query) async {
+    final response = await http.get(
+      Uri.parse('$baseUrl/search/movie?query=$query&api_key=$apikey'),
+    );
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['result']);
+  }
 }
