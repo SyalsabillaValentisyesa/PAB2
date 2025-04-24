84231ac31d64e3b2984bac249c598dbee5752b45
SyalsabillaValentisyesa <syalsabillavalentisyesa_2327250054@mhs.mdp.ac.id>
2025-04-24 11:50:28 +0700

    fasum selesai

--- fasum/lib/screen/home_screen.dart
+++ fasum/lib/screen/home_screen.dart
@@ -1,18 +1,36 @@
 import 'package:fasum/screen/sign_in_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fasum/screen/add.post_screen.dart';
+import 'dart:convert';
+import 'package:intl/intl.dart'; 
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  String formatTime(DateTime dateTime) {
+    final now = DateTime.now(); 
+    final diff = now.difference(dateTime);
+
+    if (diff.inSeconds < 60) {
+      return '${diff.inSeconds} secs ago';
+    } else if (diff.inMinutes < 60) {
+      return '${diff.inMinutes} mins ago';
+    } else if (diff.inHours < 24) {
+      return '${diff.inHours} hrs ago';
+    } else {
+      return DateFormat('dd/MM/yyyy').format(dateTime);
+    }
+  }
+
   Future<void> signOut(BuildContext context) async {
     await FirebaseAuth.instance.signOut();
 
-    Navigator.of(context).pushReplacement(
-      MaterialPageRoute(
-        builder: (context) => SignInScreen(), 
-      ),
-    );
+    Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => const SignInScreen()),
+        (route) => false);
   }
 
   @override
@@ -23,15 +41,91 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         actions: [
           IconButton(
-            onPressed: () {
-              signOut(context);
-            }, 
-            icon: Icon(Icons.logout)
-          ),
+              onPressed: () {
+                signOut(context);
+              },
+              icon: Icon(Icons.logout)),
         ],
       ),
-      body: const Center(
-        child: Text('You hame logged in'),
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('posts')
+              .orderBy('createdAt', descending: true)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData)
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+
+            final posts = snapshot.data!.docs;
+
+            return ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (context, index) {
+                final data = posts[index].data();
+                final imageBase64 = data['image'];
+                final description = data['description'];
+                final createdAtStr = data['createdAt'];
+                final fullName = data['fullName'] ?? 'Anonim';
+
+                final createdAt = DateTime.parse(createdAtStr);
+
+                return Card(
+                  margin: const EdgeInsets.all(10),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      if (imageBase64 != null)
+                        ClipRRect(
+                          borderRadius: const BorderRadius.vertical(
+                              top: Radius.circular(10)),
+                          child: Image.memory(
+                            base64Decode(imageBase64),
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                            height: 200,
+                          ),
+                        ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              formatTime(createdAt),
+                              style: const TextStyle(
+                                  fontSize: 12, color: Colors.grey),
+                            ),
+                            Text(
+                              fullName,
+                              style: const TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w500),
+                            ),
+                            const SizedBox(height: 6),
+                            Text(
+                              description ?? '',
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => AddPostScreen()),
+          );
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
