1eddbd32be3206f59df7b68c4ecfac7efb7377eb
SyalsabillaValentisyesa <syalsabillavalentisyesa_2327250054@mhs.mdp.ac.id>
2025-05-08 09:53:02 +0700

    kemaren

--- fasum/lib/main.dart
+++ fasum/lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:fasum/firebase_options.dart';
-import 'package:fasum/screen/home_screen.dart';
-import 'package:fasum/screen/sign_in_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fasum/screen/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -22,19 +20,10 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Fasum',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(), 
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return const HomeScreen();
-          } else {
-            return const SignInScreen();
-          }
-        },
-      ),
+      home: SplashScreen(),
     );
   }
 }
